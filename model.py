4388b4191913caf8adee18f0f5d946ee01ba30ef
Rhum1N <60776729+Rhum1N@users.noreply.github.com>
2025-03-25 17:14:11 +0100

    add model and simple generated dataset

---
+++ model.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+
+class BOTModel(nn.Module) :
+    def __init__(self,input_size=3,hidden_size=64,output_size=2) :
+        super(BOTModel,self).__init__()
+        self.rnn = nn.LSTM(input_size,hidden_size,batch_first=True)
+        self.fc = nn.Linear(hidden_size,output_size)
+
+    def forward(self, x) :
+        _, (hn, _) = self.rnn(x)
+        x = self.fc(hn.squeeze(0))
+        return x
+
+
+def create_sequences(positions,angles,target_positions,seq_length = 15) :
+    X,Y = [],[]
+
+    for i in range(len(angles)-seq_length) :
+        angle_seq = angles[i:i+seq_length].unsqueeze(0)  
+        sensor_seq = positions[i:i+seq_length]  
+        X.append(torch.cat([angle_seq, sensor_seq], dim=1))  # Concaténer (θ, x_s, y_s)
+        Y.append(target_positions[i+seq_length])  # Position cible
+    return torch.stack(X), torch.stack(Y)
+
+if __name__ == '__main__' :
+
+    #get the data
+    df = pd.read_csv("training.csv")
+
+    positions = torch.tensor(df[['pos_x', 'pos_y']].values, dtype=torch.float32)  # Position du capteur
+    angles = torch.tensor(df['angle'].values, dtype=torch.float32)  # Angles observés
+    target_positions = torch.tensor(df[['target_x', 'target_y']].values, dtype=torch.float32)  # Position cible
+
+    print(positions)
+    X_train, Y_train = create_sequences(angles, positions, target_positions, seq_length=5)
+    model = BOTModel(input_size=3, hidden_size=64, output_size=2)
